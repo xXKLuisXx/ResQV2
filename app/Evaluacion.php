40cbaa7b11f4867e730d97e25542ad9540eaf128
Francisco <frjesus1997@gmail.com>
2020-05-19 07:49:41 -0500

    Evaluaciones

--- app/Evaluacion.php
+++ app/Evaluacion.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Evaluacion extends Model
 {
-    //
+    public function users()
+    {
+        return $this
+            ->belongsToMany('App\User')
+            ->withTimestamps();
+    }
 }
