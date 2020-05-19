37692d5bc1b746785860173a4f7faa7bfbc6523f
Francisco <frjesus1997@gmail.com>
2020-05-18 22:42:26 -0500

    evaluaciones

--- app/User.php
+++ app/User.php
@@ -43,4 +43,43 @@ class User extends Authenticatable
             ->belongsToMany('App\Role')
             ->withTimestamps();
     }
+
+    public function authorizeRoles($roles)
+    {
+        if ($this->hasAnyRole($roles)) {
+            return true;
+        }
+        abort(401, 'Esta acción no está autorizada.');
+    }
+
+    public function hasAnyRole($roles)
+    {
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRole($role)) {
+                    return true;
+                }
+            }
+        } else {
+            if ($this->hasRole($roles)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function evaluaciones()
+    {
+        return $this
+            ->belongsToMany('App\Evaluacion')
+            ->withTimestamps();
+    }
 }
