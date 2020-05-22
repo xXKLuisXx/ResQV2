d2f37285d71aafba1eb7532733573d9bc4c741c5
LuisPerformance <proffine1@gmail.com>
2020-05-21 23:04:06 -0500

    Login
    Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,54 +2,58 @@
 @section('title', 'Inicia sesion')
 @section('content')
 
-<div class="w-full max-w-xs">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}">
-        @csrf
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                {{ __('E-Mail Address') }}
-            </label>
-            <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-            @error('email')
+<div class="flex justify-center items-center h-full">
+    <div class="">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    {{ __('E-Mail Address') }}
+                </label>
+                <input id="email" type="email"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                @error('email')
                 <span class="" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-        </div>
-        <div class="mb-6">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                {{ __('Password') }}
-            </label>
-            <input id="password" type="password" placeholder="******************"
-                class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password"
-                required autocomplete="current-password">
-            @error('password')
+                @enderror
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    {{ __('Password') }}
+                </label>
+                <input id="password" type="password" placeholder="******************"
+                    class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    name="password" required autocomplete="current-password">
+                @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
-            @enderror
-            <p class="text-red-500 text-xs italic">{{ _("Please choose a password.")}}</p>
-        </div>
-        <div class="flex items-center justify-between">
-            <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="submit">
-                {{ __('Login') }}
-            </button>
-            @if (Route::has('password.request'))
+                @enderror
+                <p class="text-red-500 text-xs italic">{{ _("Please choose a password.")}}</p>
+            </div>
+            <div class="flex items-center justify-between">
+                <button
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit">
+                    {{ __('Login') }}
+                </button>
+                @if (Route::has('password.request'))
                 <a class="btn btn-link" href="{{ route('password.request') }}">
 
                 </a>
                 <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
                     {{ __('Forgot Your Password?') }}
                 </a>
-            @endif
+                @endif
 
-        </div>
-    </form>
-    <p class="text-center text-gray-500 text-xs">
-        &copy;2020 Acme Corp. All rights reserved.
-    </p>
+            </div>
+        </form>
+        <p class="text-center text-gray-500 text-xs">
+            &copy;2020 Acme Corp. All rights reserved.
+        </p>
+    </div>
 </div>
 
 @endsection
