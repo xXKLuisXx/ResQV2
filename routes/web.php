a35aa7a628c3bfbf7f53f7364fbe3b47b426547b
Francisco <frjesus1997@gmail.com>
2020-05-26 17:38:24 -0500

    cp 2

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,10 @@ Auth::routes();
 Auth::routes(['verify' => true]);
 
 Route::resource('historia', 'HistoriaController')->shallow()->middleware(['auth','isverified']);
-Route::get('/perfil/{user}/edit', 'PerfilController@edit')->name('editPerfil')->middleware(['auth','isverified']);
-Route::get('perfil/{id_user}', 'PerfilController@show')->name('perfil')->middleware(['auth','isverified']);
 Route::resource('perfil', 'PerfilController')->except('show','edit')->middleware(['auth','isverified']);
+Route::get('perfil/{user}', 'PerfilController@show')->name('perfil')->middleware(['auth','isverified']);
+Route::get('/perfil/{user}/edit', 'PerfilController@edit')->name('editPerfil')->middleware(['auth','isverified']);
+
 
 Route::resource('imagen', 'ImagenController');
 
